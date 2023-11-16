4b35b96a170a94b776123294d3b50e39249e3535
Phamdgoon <phamdgoon1606@gmail.com>
2023-11-16 22:27:50 +0700

    api

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,8 +1,17 @@
 import express from "express";
 import customerController from "../controller/customerController";
+import homeController from "../controller/homeController";
 const router = express.Router();
 
 const initApiRoutes = (app) => {
+    // render login page
+    router.get("/", homeController.showLogin);
+    router.get("/getAllBrandCar", homeController.ShowDataDrop); //done
+    router.get("/getAllTypeCar", homeController.ShowDataDropType); //done
+    router.get("/getAllColorCar", homeController.ShowDataDropColor); //done
+
+    app.post("/sendHelp", homeController.sendHelp);
+
     router.post("/api/login", customerController.handleLogin);
     router.post("/api/register", customerController.handleRegister);
 
