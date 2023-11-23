590ae2bb45635d749a6bda8f54da9dbaa542d85c
congit27 <conbo1102a@gmail.com>
2023-11-23 15:26:38 +0700

    config port

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const webSocket = require('./src/until/webSocket');
 require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 8080;
-const WEB_SOCKET_PORT = process.env.PORT || 3000;
 
 const server = http.createServer(app);
 webSocket(server);
@@ -21,14 +20,10 @@ app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 initApiRoutes(app);
 // connectDB();
 
-server.listen(WEB_SOCKET_PORT, () => {
-    console.log(`WebSocket server listening on port ${WEB_SOCKET_PORT}`);
-});
-
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.listen(PORT, () => {
-    console.log('Backend is running on the port: ' + PORT);
+server.listen(PORT, () => {
+    console.log(`WebSocket server listening on port ${PORT}`);
 });
