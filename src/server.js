d49d6c4c7545d317239afef85e8cda5b9af823a1
congit27 <conbo1102a@gmail.com>
2023-11-23 14:56:45 +0700

    add webSocket

--- src/server.js
+++ src/server.js
@@ -1,20 +1,30 @@
-import express from "express";
-import configViewEngine from "./config/viewEngine";
-import initApiRoutes from "./routes/api";
-import bodyParser from "body-parser";
+import express from 'express';
+import configViewEngine from './config/viewEngine';
+import initApiRoutes from './routes/api';
+import bodyParser from 'body-parser';
+const http = require('http');
+const webSocket = require('./until/webSocket');
 
-require("dotenv").config();
+require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 8080;
+const WEB_SOCKET_PORT = process.env.PORT || 3000;
+
+const server = http.createServer(app);
+webSocket(server);
 
 configViewEngine(app);
 
-app.use(bodyParser.json({ limit: "50mb" }));
-app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 initApiRoutes(app);
 // connectDB();
 
+server.listen(WEB_SOCKET_PORT, () => {
+    console.log(`WebSocket server listening on port ${WEB_SOCKET_PORT}`);
+});
+
 app.listen(PORT, () => {
-    console.log("Backend is running on the port: " + PORT);
+    console.log('Backend is running on the port: ' + PORT);
 });
---
+++ src/until/webSocket.js
@@ -0,0 +1,67 @@
+const socketIo = require('socket.io');
+const sendPushNotification = require('./pushNotifications');
+
+function webSocket(server) {
+    const io = socketIo(server);
+
+    let expoPushToken = null;
+    let receiveRequestsEnabled = false;
+
+    io.on('connection', (socket) => {
+        console.log('A new client connected', socket.id);
+
+        // receive request from customer
+        socket.on('rescue-request', (data) => {
+            console.log(data);
+            if (receiveRequestsEnabled && data.location != null) {
+                !!expoPushToken && sendPushNotification(expoPushToken, 'Yêu cầu cứu hộ mới !', data.message);
+                io.emit('new-rescue-request', data);
+            }
+        });
+
+        // receive request canceling from customer
+        socket.on('cancel-rescue-request', (data) => {
+            io.emit('send-cancel-request', 'Khách hàng đã hủy yêu cầu!');
+        });
+
+        // receive expo pushs token from partner
+        socket.on('send-expo-push-token', (data) => {
+            console.log(data);
+            receiveRequestsEnabled = true;
+            expoPushToken = data.token;
+        });
+
+        // receive close connection from partner
+        socket.on('close-connect', () => {
+            console.log('Trạng thái đóng');
+            receiveRequestsEnabled = false;
+        });
+
+        // handle partner accepted
+        socket.on('accept-request', (data) => {
+            if (data.acceptedState) {
+                io.emit('accepted-request', data);
+            }
+        });
+
+        // handle partner came
+        socket.on('come-notification', (data) => {
+            if (data.isCome) {
+                io.emit('come-notification-partner', data);
+            }
+        });
+
+        // handle partner cost notice
+        socket.on('cost-notice', (data) => {
+            if (data) {
+                io.emit('cost-notice-partner', data);
+            }
+        });
+
+        socket.on('disconnect', () => {
+            console.log('A client disconnected');
+        });
+    });
+}
+
+module.exports = webSocket;
