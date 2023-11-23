c2b5ce6d09f290602288bdc761e7483f37f1d36e
TrungAnh <votrantrunganh@gmail.com>
2023-11-23 12:27:27 +0700

    add data type, color, brand

---
+++ src/migrations/create-brand.js
@@ -0,0 +1,27 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Brands', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      Name: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Brands');
+  }
+};
--- src/migrations/create-detail-service.js
+++ src/migrations/create-detail-service.js
@@ -9,7 +9,7 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       serviceId: {
-        type: Sequelize.STRING
+        type: Sequelize.INTEGER
       },
       nameServiceDetail: {
         type: Sequelize.STRING
